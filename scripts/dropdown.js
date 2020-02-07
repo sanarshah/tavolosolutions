ce1aa087ff9d3a1e26b352fb1bb35d4b86e2fbdd
alugocp <alugocp@aim.com>
2020-02-06 22:18:30 -0500

    Added fade in and new header stuff

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,8 +1,15 @@
 $(window).ready(function(){
   $(".navbar img").click(function(){
     var open=$(this).attr("src").indexOf("close")>-1;
-    $(this).attr("src",open?"res/menu.svg":"res/close.svg");
+    var pre=$(this).attr("src").indexOf("..")?"":"../";
+    $(this).attr("src",open?`${pre}res/menu.svg`:`${pre}res/close.svg`);
     var target=open?0:$(".nav-menu").outerHeight();
     $(".nav-menu-wrapper").animate({height:target},200,null);
   });
+
+  setTimeout(() => $("div.landing > div").css("opacity","1"),250);
+
+  //$(".contact").visible(() => alert("Hi lol"));
+  // Test using onscroll and calculate Y offset, screenHeight and scroll offset
+
 });
